8d31f0f5372a30b0846831780f9d47f7a753735b
J0Nreynolds <j14reynolds@gmail.com>
2017-10-28 21:36:31 -0500

    Code for equirectangular conversion

--- project4/main.m
+++ project4/main.m
@@ -8,7 +8,7 @@ exposure3 = 10;
 
 naive_hdr = makehdr_naive(ldr1, ldr2, ldr3, exposure1, exposure2, exposure3);
 
-selective_hdr = makehdr_selective(ldr1, ldr2, ldr3, exposure1, exposure2, exposure3);
+% selective_hdr = makehdr_selective(ldr1, ldr2, ldr3, exposure1, exposure2, exposure3);
 
 
 figure(1)
@@ -17,5 +17,9 @@ subplot(1,3,2), imagesc(ldr2), axis image, colormap default
 subplot(1,3,3), imagesc(ldr3), axis image, colormap default
 
 figure(2),imagesc(naive_hdr), axis image, colormap default
-figure(3),imagesc(selective_hdr), axis image, colormap default
+% figure(3),imagesc(selective_hdr), axis image, colormap default
+
+equirectangular = mirrorball2latlon(naive_hdr);
+figure(3),imagesc(equirectangular), axis image, colormap default
+imwrite(equirectangular, './results/equirectangular.jpg');
 
---
+++ project4/makehdr_response.m
@@ -0,0 +1,7 @@
+function [ output_args ] = makehdr_response( input_args )
+%MAKEHDR_RESPONSE Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+

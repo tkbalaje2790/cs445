ea0063075b4ba10000829aaca548c607a1bb51d6
J0Nreynolds <j14reynolds@gmail.com>
2017-10-29 15:55:43 -0500

    Cubemap code and results images

--- project4/main.m
+++ project4/main.m
@@ -1,3 +1,6 @@
+close all;
+colormap(parula);
+
 ldr1 = im2uint8(imread('./samples/1:50.jpg'));
 ldr2 = im2uint8(imread('./samples/1:10.jpg'));
 ldr3 = im2uint8(imread('./samples/10.jpg'));
@@ -17,9 +20,17 @@ subplot(1,3,2), imagesc(ldr2), axis image, colormap default
 subplot(1,3,3), imagesc(ldr3), axis image, colormap default
 
 figure(2),imagesc(naive_hdr), axis image, colormap default
-% figure(3),imagesc(selective_hdr), axis image, colormap default
+% figure(3),imagesc(selective_hdr), axis image, colormap cdefault
 
-equirectangular = mirrorball2latlon(naive_hdr);
-figure(3),imagesc(equirectangular), axis image, colormap default
-imwrite(equirectangular, './results/equirectangular.jpg');
+% equirectangular = mirrorball2latlon(naive_hdr);
+% figure(3),imagesc(equirectangular), axis image, colormap default
+% imwrite(equirectangular, './results/equirectangular.jpg');
+
+cubemap = mirrorball2cubemap(naive_hdr);
+figure(4),imagesc(cubemap), axis image, colormap default
+imwrite(cubemap, './results/cubemap.jpg');
+
+% cubemap = latlon2cubemap();
+% figure(4),imagesc(cubemap), axis image, colormap default
+% imwrite(cubemap, './results/cubemap.jpg');
 
--- project4/mirrorball2latlon.m
+++ project4/mirrorball2latlon.m
@@ -1,3 +1,5 @@
+
+
 function latlon = mirrorball2latlon( mirrorball_hdr )
     offset = pi/2; % offset used later to center image
 
@@ -6,13 +8,7 @@ function latlon = mirrorball2latlon( mirrorball_hdr )
     assert(rows==cols,'Mirror ball image must be square!');
     
     %Create equirectangular (latitude-longitude) image here
-    X = zeros(rows, cols);
-    temp = 1:cols;
-    X(:,:) = repmat(temp, rows, 1);
-    Y = zeros(rows, cols);
-    temp = 1:rows;
-    temp = reshape(temp, rows, 1);
-    Y(:,:) = repmat(temp, 1, cols);
+    [X, Y] = meshgrid(1:cols, 1:rows);
     X = (X-cols/2)/(cols/2); % X now holds the x coord of each pixel
     Y = -(Y-cols/2)/(cols/2); % Y now holds the y coord of each pixel
     Z = sqrt(1-(X.*X + Y.*Y)); % Z = sqrt(1-(x^2+y^2)) for each pixel
@@ -27,13 +23,14 @@ function latlon = mirrorball2latlon( mirrorball_hdr )
     % Offset phi values
     phi_ball = phi_ball+offset;
     phi_ball(phi_ball > pi) = phi_ball(phi_ball > pi) -2*pi;
+    phi_ball(phi_ball < -pi) = phi_ball(phi_ball < -pi) +2*pi;
     
     % Reshape matrices
     phi_ball = reshape(phi_ball, rows*cols, 1);
     theta_ball = reshape(theta_ball, rows*cols, 1);
     mirrorball_hdr = reshape(mirrorball_hdr, rows*cols, 1, dim);
     
-    [phis, thetas] = meshgrid([-pi:pi/360:pi], 0:pi/360:pi);
+    [phis, thetas] = meshgrid([-pi:pi/359:pi], 0:pi/359:pi);
     latlon = zeros(size(thetas));
     for i = 1:dim
         F = scatteredInterpolant(phi_ball, theta_ball, mirrorball_hdr(:,i));

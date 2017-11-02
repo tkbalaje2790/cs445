a4f2b065b5324442f81bcd4d18e5dea51c9f8fd4
Vishrut Dixit <vishrutdixit@gmail.com>
2017-11-01 23:27:46 -0500

    done

--- project4/main.m
+++ project4/main.m
@@ -18,13 +18,21 @@ colormap(parula);
 % ldr5 = im2uint8(imread('./samples/new/30.jpg'));
 % exps = [2 4 8 15 30];
 
-ldr1 = im2uint8(imread('./samples/angle1/1:30.jpg'));
-ldr2 = im2uint8(imread('./samples/angle1/1:15.jpg'));
-ldr3 = im2uint8(imread('./samples/angle1/1:8.jpg'));
-ldr4 = im2uint8(imread('./samples/angle1/1:4.jpg'));
-ldr5 = im2uint8(imread('./samples/angle1/1:2.jpg'));
+% ldr1 = im2uint8(imread('./samples/angle1/1:30.jpg'));
+% ldr2 = im2uint8(imread('./samples/angle1/1:15.jpg'));
+% ldr3 = im2uint8(imread('./samples/angle1/1:8.jpg'));
+% ldr4 = im2uint8(imread('./samples/angle1/1:4.jpg'));
+% ldr5 = im2uint8(imread('./samples/angle1/1:2.jpg'));
+% 
+% exps = [1/30 1/15 1/8 1/4 1/2];
 
-exps = [1/30 1/15 1/8 1/4 1/2];
+ldr1 = im2uint8(imread('./samples/angle2/1:20.jpg'));
+ldr2 = im2uint8(imread('./samples/angle2/1:10.jpg'));
+ldr3 = im2uint8(imread('./samples/angle2/1:5.jpg'));
+ldr4 = im2uint8(imread('./samples/angle2/0.4.jpg'));
+ldr5 = im2uint8(imread('./samples/angle2/0.8.jpg'));
+
+exps = [1/20 1/10 1/5 0.4 0.8];
 
 ldrs = cat(4, ldr1, ldr2, ldr3, ldr4, ldr5);
 
@@ -61,10 +69,6 @@ irr8 = irradiance(gsolve_hdr, 1);
 
 irrs = cat(4,irr1,irr2,irr3,irr4,irr5);
 
-naive_irr = scale_irradiance(irrs,irr6);
-selective_irr = scale_irradiance(irrs,irr7);
-gsolve_irr = scale_irradiance(irrs,irr8);
-
 %filter out -inf values from log(0)g
 irr1_filtered = irr1 .* ~isinf(irr1);
 irr2_filtered = irr2 .* ~isinf(irr2);
@@ -87,21 +91,24 @@ max5 = max(irr5(:));
 %overall min/max across all exposures
 min_val = min([min1 min2 min3 min4 min5]);
 max_val = max([max1 max2 max3 max4 max5]);
- 
-%scale
-[y,x,z] = size(irr1);
 
-for r = 1:y
-    for c = 1:x
-        for d = 1:z
-            irr1(r,c,d) = (irr1(r,c,d) - min_val) / (max_val-min_val);
-            irr2(r,c,d) = (irr2(r,c,d) - min_val) / (max_val-min_val);
-            irr3(r,c,d) = (irr3(r,c,d) - min_val) / (max_val-min_val);
-            irr4(r,c,d) = (irr4(r,c,d) - min_val) / (max_val-min_val);
-            irr5(r,c,d) = (irr5(r,c,d) - min_val) / (max_val-min_val);
-        end
-    end 
-end
+irr1 = scale_irradiance(irr1,min_val,max_val);
+irr2 = scale_irradiance(irr2,min_val,max_val);
+irr3 = scale_irradiance(irr3,min_val,max_val);
+irr4 = scale_irradiance(irr4,min_val,max_val);
+irr5 = scale_irradiance(irr5,min_val,max_val);
+imwrite(irr1, './results/irr1.jpg');
+imwrite(irr2, './results/irr2.jpg');
+imwrite(irr3, './results/irr3.jpg');
+imwrite(irr4, './results/irr4.jpg');
+imwrite(irr5, './results/irr5.jpg');
+
+naive_irr = scale_irradiance(irr6,min_val,max_val);
+imwrite(naive_irr, './results/naive_irr.jpg');
+selective_irr = scale_irradiance(irr7,min_val,max_val);
+imwrite(selective_irr, './results/selective_irr.jpg');
+gsolve_irr = scale_irradiance(irr8,min_val,max_val);
+imwrite(gsolve_irr, './results/gsolve_irr.jpg');
 
 figure(1)
 subplot(1,5,1), imagesc(irr1), axis image, colormap jet
@@ -116,24 +123,24 @@ subplot(1,3,2), imagesc(selective_irr), axis image, colormap jet
 subplot(1,3,3), imagesc(gsolve_irr), axis image, colormap jet
 
 
-% equirectangular = mirrorball2latlon(gsolve_hdr);
-% equirectangular(equirectangular< 0) = 0;
-% figure(3),imagesc(equirectangular), axis image, colormap default
-% hdrwrite(equirectangular, './results/equirectangular.hdr');
-% imwrite(tonemap(equirectangular), './results/equirectangular.jpg');
+equirectangular = mirrorball2latlon(gsolve_hdr);
+equirectangular(equirectangular< 0) = 0;
+figure(3),imagesc(equirectangular), axis image, colormap default
+hdrwrite(equirectangular, './results/equirectangular.hdr');
+imwrite(tonemap(equirectangular), './results/equirectangular.jpg');
 
-% cubemap = mirrorball2cubemap(gsolve_hdr);
-% cubemap(~isfinite(cubemap)) = 1;
-% figure(4),imagesc(cubemap), axis image, colormap default
-% hdrwrite(cubemap, './results/cubemap.hdr');
-% imwrite(tonemap(cubemap), './results/cubemap.jpg');
+cubemap = mirrorball2cubemap(gsolve_hdr);
+cubemap(~isfinite(cubemap)) = 1;
+figure(4),imagesc(cubemap), axis image, colormap default
+hdrwrite(cubemap, './results/cubemap.hdr');
+imwrite(tonemap(cubemap), './results/cubemap.jpg');
 
 % cubemap = latlon2cubemap();
 % figure(4),imagesc(cubemap), axis image, colormap default
 % imwrite(cubemap, './results/cubemap.jpg');
 
-% angular = mirrorball2angular(gsolve_hdr);
-% figure(4),imagesc(angular), axis image, colormap default
-% hdrwrite(angular, './results/angular.hdr');
-% imwrite(tonemap(angular), './results/angular.jpg');
+angular = mirrorball2angular(gsolve_hdr);
+figure(4),imagesc(angular), axis image, colormap default
+hdrwrite(angular, './results/angular.hdr');
+imwrite(tonemap(angular), './results/angular.jpg');
 

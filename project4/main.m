6996fdca40de1f5ab234a53125089ff93f113240
Vishrut Dixit <vishrutdixit@gmail.com>
2017-11-01 22:54:30 -0500

    fix

--- project4/main.m
+++ project4/main.m
@@ -81,8 +81,8 @@ max4 = max(irr4(:));
 max5 = max(irr5(:));
 
 %overall min/max across all exposures
-min_val = min([min1 min2 min3 min4 min5 min6 min7 min8]);
-max_val = max([max1 max2 max3 max4 max5 max6 max7 max8]);
+min_val = min([min1 min2 min3 min4 min5]);
+max_val = max([max1 max2 max3 max4 max5]);
  
 %scale
 [y,x,z] = size(irr1);
